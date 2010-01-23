8b40d1b7d7d9fd61c6806bb44b5b3f2a52d542a2
first.team1100 <first.team1100@993e3576-07d5-11df-8158-c7cbc0dd6240>
2010-01-23 06:27:23 +0000

    Initial template commit.

---
+++ trunk/src/edu/wpi/first/wpilibj/templates/DashboardPacker.java
@@ -0,0 +1,9 @@
+package edu.wpi.first.wpilibj.templates;
+
+/**
+ *
+ * @author Michael
+ */
+public class DashboardPacker {
+
+}
---
+++ trunk/src/edu/wpi/first/wpilibj/templates/Robot1100.java
@@ -0,0 +1,71 @@
+package edu.wpi.first.wpilibj.templates;
+
+
+import edu.wpi.first.wpilibj.IterativeRobot;
+
+/**
+ * The VM is configured to automatically run this class, and to call the
+ * functions corresponding to each mode, as described in the IterativeRobot
+ * documentation. If you change the name of this class or the package after
+ * creating this project, you must also update the manifest file in the resource
+ * directory.
+ */
+public class Robot1100 extends IterativeRobot
+{
+    /**
+     * This function is run when the robot is first started up and should be
+     * used for any initialization code.
+     */
+    public void robotInit()
+    {
+
+    }
+
+    /**
+     * This function is called when the robot enters autonomous mode.
+     */
+    public void autonomousInit()
+    {
+
+    }
+
+    /**
+     * This function is called periodically (100Hz) during autonomous
+     */
+    public void autonomousPeriodic()
+    {
+
+    }
+
+    /**
+     * This function is called when the robot enters teleop mode.
+     */
+    public void teleopInit()
+    {
+
+    }
+
+    /**
+     * This function is called periodically (100Hz) during operator control
+     */
+    public void teleopPeriodic()
+    {
+        
+    }
+
+    /**
+     * This function is called when the robot enters disabled mode.
+     */
+    public void disabledInit()
+    {
+
+    }
+
+    /**
+     * This function is called periodically (100Hz) during disabled mode.
+     */
+    public void disabledPeriodic()
+    {
+
+    }
+}

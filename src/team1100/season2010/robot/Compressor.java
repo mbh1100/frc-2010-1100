b597128eaf8627193fe2f697c70b1fd71110ab06
First.Team1100 <First.Team1100@993e3576-07d5-11df-8158-c7cbc0dd6240>
2010-02-01 05:00:53 +0000

    Class to encapsulate compressor sequencing.

---
+++ src/team1100/season2010/robot/Compressor.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package team1100.season2010.robot;
+
+/**
+ *
+ * @author mark
+ */
+public class Compressor {
+
+    public Compressor() {}
+
+    public void start() {}
+
+    public void stop() {}
+
+    public int getOutputPressure() { return 0; }
+    
+}

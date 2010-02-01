1c15aee184c01444b38657669aedabd9b93a599b
First.Team1100 <First.Team1100@993e3576-07d5-11df-8158-c7cbc0dd6240>
2010-02-01 04:57:53 +0000

    Class to encapsulate kicker sequencing

---
+++ src/team1100/season2010/robot/Kicker.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package team1100.season2010.robot;
+
+/**
+ *
+ * @author mark
+ */
+public class Kicker
+{
+    public Kicker() {}
+
+    public void prepareHardKick() {}
+
+    public void prepareSoftKick() {}
+
+    public void kick() {}
+
+    public boolean isReady() { return false; }
+}

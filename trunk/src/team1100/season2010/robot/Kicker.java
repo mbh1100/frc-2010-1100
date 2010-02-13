0e856cd5cb14d32690a8805556a5878d95df4419
koala_bear@verizon.net <koala_bear@verizon.net@993e3576-07d5-11df-8158-c7cbc0dd6240>
2010-02-13 01:47:34 +0000

    Updated kicker code.  -Bianca

--- trunk/src/team1100/season2010/robot/Kicker.java
+++ trunk/src/team1100/season2010/robot/Kicker.java
@@ -19,6 +19,7 @@ public class Kicker
 {
   private boolean hardKick;
   private boolean primed;
+  private int prev_count;
   private Compressor compressor;
   private Solenoid valve_1_A;
   private Solenoid valve_1_B;
@@ -26,23 +27,22 @@ public class Kicker
   private Solenoid valve_2_B;
   private Solenoid valve_3_B;
   private Solenoid valve_3_A;
-  private DigitalInput latch_mount;
-  private DigitalInput latch_piston;
-  private DigitalInput kick_mount;
-  private DigitalInput kick_piston;
+  //private DigitalInput latch_mount;
+  private DigitalInput limit_switch;
+ // private DigitalInput latch_piston;
+ // private DigitalInput kick_mount;
+ // private DigitalInput kick_piston;
 
   private int prime_state;
-  private int kick_state;
   private int test_state;
 
 
   public Kicker()
   {
     hardKick = true;
-    primed = false;
     prime_state = 0;
-    kick_state = 0;
     test_state = 0;
+    prev_count = 0;
 
     valve_1_A = new Solenoid(8,1);
     valve_2_A = new Solenoid(8,3);
@@ -51,15 +51,24 @@ public class Kicker
     valve_2_B = new Solenoid(8,4);
     valve_3_B = new Solenoid(8,6);
 
-    latch_mount = new DigitalInput(4,4);
-    latch_piston = new DigitalInput(4,3);
-    kick_mount = new DigitalInput(4,2);
-    kick_piston = new DigitalInput(4,1);
+    //latch_mount = new DigitalInput(4,4);
+    limit_switch = new DigitalInput(4,4);
+   // latch_piston = new DigitalInput(4,3);
+    //kick_mount = new DigitalInput(4,2);
+   // kick_piston = new DigitalInput(4,1);
+
+    if(limit_switch.get())
+        primed = true;
+    else primed = false;
 
     compressor = new Compressor(4,5,4,1);
-    depressurize();
     compressor.start();
     primeKicker();
+  }
+
+  public void turnOnKicker()
+  {
+      compressor.start();
   }
 
   public void setHardSoft(boolean hard_or_soft)
@@ -70,13 +79,17 @@ public class Kicker
     if(primed)
       if(hardKick)
       {
+        valve_1_A.set(false);
         valve_1_B.set(true);
+        valve_3_A.set(false);
         valve_3_B.set(true);
       }
       else
       {
-        valve_1_B.set(false);
+        valve_1_A.set(false);
+        valve_1_B.set(true);
         valve_3_B.set(false);
+        valve_3_A.set(true);
       }
   }
 
@@ -89,175 +102,125 @@ public class Kicker
   {
     if(!primed)
     {
-      //DEPRESSURIZE FIRST
       if(prime_state == 0)
       {
-        depressurize();
+        valve_2_B.set(false);
+        valve_2_A.set(true);
+        System.out.println("open latch");
+        prime_state++;
       }
-
-      //OPEN LATCH
       else if(prime_state == 1)
       {
-        valve_2_B.set(false);
-        prime_state++;
-      }
-      else if(prime_state == 2)
-      {
-        valve_2_A.set(true);
-        prime_state++;
-      }
-      else if(prime_state == 3)
-      {
-        if(!latch_piston.get())
+       // if(!latch_piston.get())
+          System.out.println("latch open");
           prime_state++;
       }
 
       //ARM KICKER
-      else if(prime_state == 4)
+      else if(prime_state == 2)
       {
         valve_1_B.set(false);
-        valve_3_B.set(false);
-        prime_state++;
-      }
-      else if(prime_state == 5)
-      {
         valve_1_A.set(true);
+        System.out.println("arm kicker");
         prime_state++;
       }
-      else if(prime_state == 6)
+      else if(prime_state == 3)
       {
-        if(!kick_piston.get())
-          prime_state++;
+      //  if(!kick_piston.get())
+          if(limit_switch.get())
+          {
+            System.out.println("kicker armed");
+            prime_state++;
+          }
       }
 
       //LOCK LATCH
-      else if(prime_state == 7)
+      else if(prime_state == 4)
       {
         valve_2_A.set(false);
-        prime_state++;
-      }
-      else if(prime_state == 8)
-      {
         valve_2_B.set(true);
+        prime_state++;
+        System.out.println("lock latch");
       }
 
       //SET KICKER
-      else if(prime_state == 9)
+      else if(prime_state == 5)
       {
-        valve_1_A.set(false);
-        prime_state++;
-      }
-      else if(prime_state == 10)
-      {
+          System.out.println("set kicker");
         if(hardKick)
         {
+          valve_1_A.set(false);
           valve_1_B.set(true);
+          valve_3_A.set(false);
           valve_3_B.set(true);
         }
+        else
+        {
+            valve_1_A.set(false);
+            valve_1_B.set(true);
+            valve_3_B.set(false);
+            valve_3_A.set(true);
+        }
+          System.out.println("kicker set");
         primed = true;
+        prime_state = 0;
       }
     }
 
 
   }
 
-  public void kick()
+  public void kick(int curr_count)
   {
-    if(primed == true)
+    if(primed == true && curr_count - prev_count >= 200)
     {
-      if(kick_state == 0)
-      {
-        /*if(kickHard)
-          {
-            valve_1_B.set(true);
-            valve_3_B.set(true);
-          }*/
-
         valve_2_B.set(false);
-        kick_state++;
-      }
-
-      else if(kick_state == 1)
-      {
         valve_2_A.set(true);
-        kick_state++;
-      }
-
-      else if(kick_state == 2)
-      {
+        
+        prime_state = 3;
+ 
         primed = false;
-        prime_state = 0;
-        kick_state = 0;
-      }
+        prev_count = curr_count;
+
+        valve_2_A.set(false);
+        valve_2_B.set(true);
     }
     else System.out.println("ERR: Kick without prime.");
-  }
-
-  private void depressurize()
-  {
-    valve_1_A.set(false);
-    valve_1_B.set(false);
-    valve_2_A.set(false);
-    valve_2_B.set(false);
-    valve_3_A.set(false);
-    valve_3_B.set(false);
   }
 
   public void disarm()
   {
     compressor.stop();
-    depressurize();
   }
 
   public void testSolenoidSignal()
   {
     if(test_state == 0)
-    {//depressurize
-      depressurize();
+    {
+      valve_2_B.set(false);
+      valve_2_A.set(true);
+      System.out.println("Latch piston rod extended.");
       test_state++;
     }
     else if(test_state == 1)
-    {//extend kicker rod
+    {
       valve_3_B.set(false);
       valve_1_B.set(false);
-      test_state++;
-    }
-    else if(test_state == 2)
-    {
       valve_1_A.set(true);
       System.out.println("Kicker piston rod extended.");
       test_state++;
     }
-    else if(test_state == 3)
-    {//retract kicker rod
-      valve_1_A.set(false);
-      test_state++;
-    }
-    else if(test_state == 4)
+    else if(test_state == 2)
     {
+      valve_1_A.set(false);
       valve_3_B.set(true);
       valve_1_B.set(true);
       System.out.println("Kicker piston rod retracted.");
       test_state++;
     }
-    else if(test_state == 5)
-    {//extend latch rod
-      valve_2_B.set(false);
-      test_state++;
-    }
-    else if(test_state == 6)
+    else if(test_state == 3)
     {
-      valve_2_A.set(true);
-      System.out.println("Latch piston rod extended.");
-      test_state++;
-    }
-    else if(test_state == 7)
-    {//retract latch rod
       valve_2_A.set(false);
-      test_state++;
-    }
-    else if(test_state == 8)
-    {
       valve_2_B.set(true);
       System.out.println("Latch piston rod retracted.");
       test_state = 0;

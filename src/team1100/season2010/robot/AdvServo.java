8e77d9431028e8ab1a0abdffcb5b631a35aab593
koala_bear@verizon.net <koala_bear@verizon.net@993e3576-07d5-11df-8158-c7cbc0dd6240>
2010-02-19 17:59:10 +0000

    AdvServo class in order to make the servo act like a motor (it's still instantiated as a servo, but this class controls setpoint to give it a motor-like interface from a software and output perspective) and enable free-ing the servo to lift up the lift.  -Bianca

---
+++ src/team1100/season2010/robot/AdvServo.java
@@ -0,0 +1,55 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package team1100.season2010.robot;
+
+import edu.wpi.first.wpilibj.Servo;
+
+/**
+ *
+ * @author team1100
+ */
+public class AdvServo extends Servo
+{
+
+  private double servoSetpoint;
+  private double increment;
+  private double servo_max;
+  private double servo_min;
+
+  public AdvServo()
+  {
+      this(4,10);
+  }
+
+  public AdvServo(int slot, int channel)
+  {
+      super(slot,channel);
+      servoSetpoint = .5;
+      super.set(servoSetpoint);
+      increment = .005;
+      servo_max = .8;
+      servo_min = .2;
+  }
+
+  public void set(boolean direction)  //true = forward, false = backward
+  {
+      if(direction && servoSetpoint + increment < servo_max)
+      {
+          servoSetpoint += increment;
+          super.set(servoSetpoint);
+      }
+      if(!direction && servoSetpoint - increment > servo_min)
+      {
+          servoSetpoint -= increment;
+          super.set(servoSetpoint);
+      }
+  }
+
+  public void free()
+  {
+      super.free();
+  }
+}

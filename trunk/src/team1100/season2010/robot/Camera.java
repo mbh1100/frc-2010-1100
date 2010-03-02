67648439ecc05a91609e330e791090e0279a38dc
koala_bear@verizon.net <koala_bear@verizon.net@993e3576-07d5-11df-8158-c7cbc0dd6240>
2010-03-02 01:45:58 +0000

    camera wrapper class

---
+++ trunk/src/team1100/season2010/robot/Camera.java
@@ -0,0 +1,73 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package team1100.season2010.robot;
+
+/**
+ *
+ * @author team1100
+ */
+
+import edu.wpi.first.wpilibj.camera.AxisCamera;
+import edu.wpi.first.wpilibj.camera.AxisCameraException;
+import edu.wpi.first.wpilibj.image.ColorImage;
+import edu.wpi.first.wpilibj.image.NIVisionException;
+import java.util.TimerTask;
+
+public class Camera {
+
+    public Camera()
+    {
+        int  comp = AxisCamera.getInstance().getCompression();
+
+        System.out.println("jpeg compression =" + comp);
+        AxisCamera.getInstance().writeRotation(AxisCamera.RotationT.k0);
+//        AxisCamera.getInstance().writeCompression(75);
+        AxisCamera.getInstance().writeResolution(AxisCamera.ResolutionT.k320x240);
+         m_controlLoop = new java.util.Timer();
+          m_controlLoop.schedule(new CameraTask(this), 0L, (long) (1000));
+    }
+    
+     java.util.Timer m_controlLoop;
+
+    private class CameraTask extends TimerTask {
+        
+
+        private Camera m_controller;
+
+        public CameraTask(Camera controller) {
+            if (controller == null) {
+                throw new NullPointerException("Given PIDController was null");
+            }
+            m_controller = controller;
+        }
+
+        public void run() {
+            m_controller.camera_get_image();
+        }
+    }
+
+    public void camera_get_image()
+    {
+
+      if (AxisCamera.getInstance().freshImage())
+            {
+                try
+                {
+                    System.out.println("image!");
+                    ColorImage image = AxisCamera.getInstance().getImage();
+                    Thread.yield();
+                    image.free();
+                }
+                catch (NIVisionException ex)
+                {
+                }
+                catch (AxisCameraException ex)
+                {
+                }
+            }
+    }
+
+}
